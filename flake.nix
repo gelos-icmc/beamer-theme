bcb81c476202f516abea4fbc83cbe8190cd355cb
Gabriel Fontes <hi@m7.rs>
2024-01-17 10:57:16 -0300

    rework flake
    add derivation for sample pdf too

--- flake.nix
+++ flake.nix
@@ -15,11 +15,43 @@
       (system: f nixpkgs.legacyPackages.${system});
   in {
     packages = eachSystem (pkgs: rec {
-      default = pkgs.callPackage ./default.nix {};
-      texlive = pkgs.texlive.withPackages (_: default.tlDeps ++ [default]);
-    });
-    devShells = eachSystem (pkgs: {
-      default = pkgs.callPackage ./shell.nix {};
+      # Tex package
+      default = theme;
+      theme = pkgs.stdenvNoCC.mkDerivation rec {
+        pname = "gelos-beamer";
+        version = self.shortRev or "unstable";
+        src = ./.;
+        outputs = ["tex"];
+        passthru = {
+          tlDeps = with pkgs.texlive; [
+            scheme-basic
+            fancyvrb
+            beamer
+            xkeyval
+            fira
+            fontaxes
+          ];
+          tlType = "run";
+        };
+        nativeBuildInputs = [
+          (pkgs.texlive.withPackages (_: passthru.tlDeps))
+          (pkgs.writeShellScript "force-tex-output.sh" ''out="''${tex-}"'')
+        ];
+        installPhase = "install -D beamerthemegelos.sty gelos-dark.png gelos-light.png -t $tex/tex/latex/gelosbeamer";
+      };
+
+      # Texlive environment containing the theme
+      texlive-env = pkgs.texlive.withPackages (_: default.tlDeps ++ [default]);
+
+      # A sample pdf using the theme
+      sample = pkgs.stdenvNoCC.mkDerivation {
+        name = "sample";
+        src = ./.;
+        version = self.shortRev or "unstable";
+        nativeBuildInputs = [pkgs.pandoc texlive-env];
+        buildPhase = "pandoc -t beamer sample.md -o sample.pdf";
+        installPhase = "install -D sample.pdf -t $out";
+      };
     });
     formatter = eachSystem (pkgs: pkgs.alejandra);
   };
